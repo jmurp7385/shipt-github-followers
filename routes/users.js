62a7997a8bb8acf6e2f38658719f361dad27c17c
jmurp7385 <jmurp7385@gmail.com>
2017-09-29 20:49:25 -0500

    updates

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var GitHubApi = require("github");
+var github = new GitHubApi({
+  
 });
+
+/* GET users data. */
+router.post('/', function(req, res, next) {
+  github.users.getForUser({
+    username: req.body.username
+  }, function (e, r) {
+    var json = JSON.parse(JSON.stringify(r));
+    var data = json['data']    
+    console.log(data['login'],data['followers']);    
+    getFollowers(req,res,next,data);    
+    // res.render('users', {handle: data['login'], follower_count: data['followers'], userdata: data});
+  });
+  
+});
+
+function getFollowers(req,res,next,data) {
+  github.users.getFollowersForUser({
+    username: data['login']
+  }, function(e, r) {
+    var json = JSON.parse(JSON.stringify(r));
+    var followers = json['data'];
+    if (github.hasNextPage(r)) {
+            github.getNextPage(r, function(err, res) {
+                followers += JSON.parse(JSON.stringify(res))['data'];
+            });
+        }
+    res.render('users', {handle: data['login'], follower_count: data['followers'], followers: followers});
+  });
+}
 
 module.exports = router;
